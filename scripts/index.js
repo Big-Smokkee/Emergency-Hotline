38cf41f9cfafb37aaec56cda488d0764aa3b60e2
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-11-21 17:36:11 +0600

    feat: added functionality for heart incrementation and coin decrementation with faulty alert.Need to fix it

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,35 @@
-const heartSpan = document.getElementsByClassName("heart-span");
-console.log(heartSpan);
-const heartIncrementor = document.getElementById("heart-incrementor");
-console.log(heartIncrementor.innerText);
-const coinDecrementor = document.getElementById("coin-decrementor");
-console.log(coinDecrementor.innerText);
+//function for incrementation of hearts and decrementing coins
+function getElementValue(id) {
+    return parseInt(document.getElementById(id).innerText);
+}
+function setElementValue(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
+let heartSpan = document.getElementsByClassName("heart-span");
+let callSpan = document.getElementsByClassName("call-span");
+
+let heartIncrementor = getElementValue("heart-incrementor");
+let coinDecrementor = getElementValue("coin-decrementor");
+
+
 
 for (const heart of heartSpan) {
     heart.addEventListener("click", function () {
-        heartIncrementor.innerText++;
-        console.log(heartIncrementor.innerText);
+
+        heartIncrementor = heartIncrementor + 1;
+        setElementValue("heart-incrementor", heartIncrementor);
+    })
+}
+for (const btn of callSpan) {
+    btn.addEventListener("click", function () {
+        if (coinDecrementor <= 0) {
+            alert("Low Credit");
+        }
+        else {
+            coinDecrementor -= 20;
+            setElementValue("coin-decrementor", coinDecrementor);
+            alert("done");
+        }
     })
 }
